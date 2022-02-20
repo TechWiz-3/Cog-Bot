a2a2e63413fc9dab04ba740e681ee0b1b8cb3d77
TechWiz-3 <programmingplus1@gmail.com>
2022-02-21 10:23:41 +1100

    updated main.py

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from discord.ext import tasks
 from discord.commands import Option
 from dotenv import load_dotenv
 import os
-import random
 
-# Gravity Destroyers secret security bot
+# A bot to mess with slash commands and cogs
 
-bot = commands.Bot(command_prefix="a!")
+bot = commands.Bot(command_prefix="c!")
 
 load_dotenv()
 BOT_TOKEN = os.getenv("A_TOKEN")
@@ -16,19 +15,17 @@ BOT_TOKEN = os.getenv("A_TOKEN")
 @bot.event
 async def on_ready():
     print("I'm ready")
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Rowan"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Pycord"))
 
 
 @bot.command()
-async def halp(ctx, mod=None):
-    if mod == None:
-        await ctx.send("myhelpcmd")
-    if mod == "mod":
-        await ctx.send("Wow, now you've found out about my secrety security functions")
+async def halp(ctx):
+    await ctx.send("This ain't a help command, sorry lol")
 
 @bot.slash_command(guild_ids=[867597533458202644])  # create a slash command for the supplied guilds
 async def hello(ctx):
     """Say hello to the bot"""  # the command description can be supplied as the docstring
     await ctx.respond(f"Hello {ctx.author}! :blush:")
 
-bot.load_extension('cog')
+bot.load_extension('cog') # load cogs from cog.py
+bot.run(BOT_TOKEN)
